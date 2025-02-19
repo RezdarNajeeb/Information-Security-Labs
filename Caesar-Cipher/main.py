bd29251d6c8686721285834d4ed8dc4f0ec2b437
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-02-19 22:42:33 +0300

    Initial Commit

---
+++ Caesar-Cipher/main.py
@@ -0,0 +1,7 @@
+# Caesar Cipher Encryption Rule: C(P,K) = (P+K) % 256
+
+
+# Caesar Cipher Decryption Rule: C(P,K) = (P-K) % 256
+
+
+# Brute Force Attack On Caesar Cipher
---
+++ Monoaplphabetic-Cipher/main.py
@@ -0,0 +1,9 @@
+# Monoalphabetic Encryption Steps:
+# 1. Read each char from the plain text
+# 2. Find position of that char in ASCII
+# 3. Substitute with the char at that position in the Randomized Key
+
+
+
+# Monoaplphabetic Decryption Steps:
+# The encryption steps but reversely
