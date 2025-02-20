e24debd1eda3dc4be84243b2e86a2f3fddea97bf
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-02-20 15:21:01 +0300

    Monoalphabetic decryption done

--- Monoaplphabetic-Cipher/main.py
+++ Monoaplphabetic-Cipher/main.py
@@ -7,3 +7,19 @@
 
 # Monoaplphabetic Decryption Steps:
 # The encryption steps but reversely
+
+import random
+import string
+
+chars = list(string.ascii_letters + string.digits + string.punctuation + " ")
+random.seed(1)
+random_key = chars.copy()
+random.shuffle(random_key)
+
+def monoalphabetic_decrypt (cipher_text, key):
+    plain_text = ""
+    for char in cipher_text:
+        plain_text += chars[key.index(char)]
+    print(f"Plain text: {plain_text}")
+
+monoalphabetic_decrypt(input("Enter cipher text: "), random_key)
