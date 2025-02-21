d2c99518108ca6707d58ae83b417f0435d35b2c5
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-02-22 02:16:50 +0300

    Implement Caesar cipher encryption, decryption, and brute force attack functionality

--- Caesar-Cipher/main.py
+++ Caesar-Cipher/main.py
@@ -1,7 +1,40 @@
-# Caesar Cipher Encryption Rule: C(P,K) = (P+K) % 256
+# Create a list of only printable characters
+chars = list(chr(i) for i in range(256) if chr(i).isprintable())
+char_map = dict(zip(chars, range(len(chars)))) # Dictionary mapping (character, index) pairs
 
+def caesar_encrypt(plain_text, shift):
+    ciphertext = []
+    for char in plain_text:
+        if char not in char_map:
+            exit(f'Invalid Character')
+        ciphertext.append(chars[(char_map[char] + shift) % len(chars)])
+    return ''.join(ciphertext)
 
-# Caesar Cipher Decryption Rule: C(P,K) = (P-K) % 256
+def caesar_decrypt(cipher_text, shift):
+    plaintext = []
+    for char in cipher_text:
+        if char not in char_map:
+            exit(f'Invalid Character')
+        plaintext.append(chars[(char_map[char] - shift) % len(chars)])
+    return ''.join(plaintext)
 
+def caesar_attack(cipher_text):
+    for i in range(len(chars)):
+        decrypted_attempt = caesar_decrypt(cipher_text, i)
+        print(f"Shift (K) = {i}: {decrypted_attempt}")
 
-# Brute Force Attack On Caesar Cipher
+# Get user input
+plainText = input("Enter the plaintext: ")
+shiftNum = abs(int(input("Enter the shift: "))) % len(chars)
+
+# Encrypt the plaintext and display the result
+encrypted = caesar_encrypt(plainText, shiftNum)
+print("Encrypted text:", encrypted)
+
+# Decrypt the encrypted text and display the result
+decrypted = caesar_decrypt(encrypted, shiftNum)
+print(f"Decrypted text: {decrypted}")
+
+# Brute force attack on the encrypted text
+print("\nBrute Force Attack")
+caesar_attack(encrypted)
